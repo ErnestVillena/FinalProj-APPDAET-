c5f3c4c483678f2b79d57311cb0494a37761d83e
ErnestVillena <ernestandre.villena@benilde.edu.ph>
2025-06-18 12:20:46 +0800

    Update models.py

--- idsystem/idType/models.py
+++ idsystem/idType/models.py
@@ -6,8 +6,9 @@ class IdType(models.Model):
     """
     Model representing an ID type.
     """
-    name = models.CharField(max_length=100, unique=True, verbose_name="ID Type Name")
-    description = models.TextField(blank=True, null=True, verbose_name="Description")
+    idType_name = models.CharField(max_length=100, unique=True, verbose_name="ID Type Name")
+    idType_desc = models.TextField(blank=True, null=True, verbose_name="Description")
+    
 
     class Meta:
         verbose_name = "ID Type"
@@ -15,7 +16,7 @@ class IdType(models.Model):
 
     def __str__(self):
         return self.name
-    
+"""    
 class IdTypeAlias(models.Model):
     """
     Model representing an alias for an ID type.
@@ -28,6 +29,6 @@ class IdTypeAlias(models.Model):
         verbose_name = "ID Type Alias"
         verbose_name_plural = "ID Type Aliases"
         unique_together = ('title', 'id_type')
-    
+"""    
     def __str__(self):
         return f"{self.title} ({self.id_type.name})"
