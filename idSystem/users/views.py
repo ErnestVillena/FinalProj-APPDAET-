6d4ced1431a3e4e2a83095e270a94d725b55d97e
ErnestVillena <ernestnandre.villena@benilde.edu.ph>
2025-07-16 12:06:33 +0800

    created login for users

--- idSystem/users/views.py
+++ idSystem/users/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def register_view(request):
@@ -7,7 +8,18 @@ def register_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("/")
+            return HttpResponse('registration successful')
+            #return redirect("/home.html")
     else:
         form = UserCreationForm()
     return render(request, "users/register.html", { "form": form })
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return HttpResponse('login successful')
+            #return redirect("/home.html")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", { "form": form })
