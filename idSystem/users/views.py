a97c5f54a3969daf66e52125d840a2dd25a7f27a
ErnestVillena <ernestnandre.villena@benilde.edu.ph>
2025-08-12 18:55:29 +0800

    added logout button, added username display on login

--- idSystem/users/views.py
+++ idSystem/users/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
@@ -17,8 +19,14 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            return HttpResponse('login successful')
-            #return redirect("/home.html")
+            login(request, form.get_user())
+            messages.success(request, ("Successfully Logged In"))
+            return redirect ('/id')
     else:
         form = AuthenticationForm()
-    return render(request, "users/login.html", { "form": form })
+        return render(request, "users/login.html", { "form": form })
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, ("Successfully Logged Out."))
+    return redirect('/')
